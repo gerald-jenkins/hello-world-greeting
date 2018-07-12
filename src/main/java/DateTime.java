9002d44b8ebfdc904bf370396819d921d8608cc0
gerald-jenkins <39489193+gerald-jenkins@users.noreply.github.com>
2018-07-12 16:03:51 +0200

    Update DateTime.java

--- src/main/java/DateTime.java
+++ src/main/java/DateTime.java
@@ -1,13 +1,14 @@
 package hello;
 
-#import java.util.Calendar;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class DateTime {
 
   public int dateTime(){
   GregorianCalendar time = new GregorianCalendar();
-  int hour = time.get(Calendar.HOUR_OF_DAY);
+#  int hour = time.get(Calendar.HOUR_OF_DAY);
+  int hore = time.get(Calendar.HOUR_OF_DAY);
   int min = time.get(Calendar.MINUTE);
   int day = time.get(Calendar.DAY_OF_MONTH);
   int month = time.get(Calendar.MONTH) + 1;
